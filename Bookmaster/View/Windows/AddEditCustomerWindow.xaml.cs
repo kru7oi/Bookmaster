f596649f212f15c79857abc2d3bbe274a2344f99
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-09 21:31:52 +0300

    Добавлено окно "Добавить/редактировать читателя"

---
+++ Bookmaster/View/Windows/AddEditCustomerWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bookmaster.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditCustomerWindow.xaml
+    /// </summary>
+    public partial class AddEditCustomerWindow : Window
+    {
+        public AddEditCustomerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
