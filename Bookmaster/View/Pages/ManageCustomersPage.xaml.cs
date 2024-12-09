f2b232061aa720a5ef8c4c9a06e2e70d3f4d9834
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-09 21:30:34 +0300

    Добавлена страница "Управление читателями"

---
+++ Bookmaster/View/Pages/ManageCustomersPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Bookmaster.Model;
+using Bookmaster.View.Windows;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Bookmaster.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ManageCustomersPage.xaml
+    /// </summary>
+    public partial class ManageCustomersPage : Page
+    {
+        private readonly List<Customer> _customers = App.context.Customers.ToList();
+        public ManageCustomersPage()
+        {
+            InitializeComponent();
+
+            CustomersLv.ItemsSource = _customers;
+        }
+
+        private void SearchCustomerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditCustomerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditCustomerWindow addEditCustomerWindow = new AddEditCustomerWindow();
+            addEditCustomerWindow.ShowDialog();
+        }
+
+        private void AddCustomerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditCustomerWindow addEditCustomerWindow = new AddEditCustomerWindow();
+            addEditCustomerWindow.ShowDialog();
+        }
+    }
+}
