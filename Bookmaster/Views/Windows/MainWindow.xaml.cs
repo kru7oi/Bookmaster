103c66c51c02e2bae799a2d2d31c30e81e0d430a
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-16 17:40:56 +0300

    Добавлено открытие окон и страниц

--- Bookmaster/Views/Windows/MainWindow.xaml.cs
+++ Bookmaster/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Bookmaster.View.Pages;
+using Bookmaster.View.Windows;
+using System.Windows;
 
 namespace Bookmaster
 {
@@ -10,6 +12,44 @@ namespace Bookmaster
         public MainWindow()
         {
             InitializeComponent();
+
+            MainFrame.Navigate(new BrowseCatalogPage());
+        }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseAppMi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BrowseCatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new BrowseCatalogPage());
+        }
+
+        private void CustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ManageCustomersPage());
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CirculationPage());
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ReportsPage());
         }
     }
 }
