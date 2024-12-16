b89b6cd03f88db736f4e0743279264d71172bef7
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-16 21:59:28 +0300

    Реализован вывод книг и авторов в список, реализован поиск по этому списку (без жанров)

--- Bookmaster/Views/Pages/BrowseCatalogPage.xaml.cs
+++ Bookmaster/Views/Pages/BrowseCatalogPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Bookmaster.Model;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Bookmaster.View.Pages
@@ -8,28 +9,39 @@ namespace Bookmaster.View.Pages
     /// </summary>
     public partial class BrowseCatalogPage : Page
     {
+        private List<BookAuthor> _bookAuthors = App.context.BookAuthors.ToList();
         public BrowseCatalogPage()
         {
             InitializeComponent();
+
+            BookAuthorsLv.ItemsSource = _bookAuthors;
+
+            BookDetailsGrid.Visibility = Visibility.Hidden;
         }
 
         private void BookAuthorsLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-        }
-
-        private void PreviousPageBtn_Click(object sender, RoutedEventArgs e)
-        {
 
         }
 
-        private void NextPageBtn_Click(object sender, RoutedEventArgs e)
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
         {
+            string bookTitle = SearchByBookTitleTb.Text;
+            string authorName = SearchByAuthorNameTb.Text;
+            string bookSubjects = SearchByBookSubjectsTb.Text;
 
-        }
-
-        private void OpenBookAuthorsDetailsHl_Click(object sender, RoutedEventArgs e)
-        {
-
+            if (string.IsNullOrEmpty(bookTitle) &&
+                string.IsNullOrEmpty(authorName) &&
+                string.IsNullOrEmpty(bookSubjects))
+            {
+                BookAuthorsLv.ItemsSource = _bookAuthors;
+            }
+            else
+            {
+                BookAuthorsLv.ItemsSource = _bookAuthors.Where(bookAuthor =>
+                bookAuthor.Book.Title.Contains(bookTitle, StringComparison.OrdinalIgnoreCase) &&
+                bookAuthor.Author.Name.Contains(authorName, StringComparison.OrdinalIgnoreCase));
+            }
         }
     }
 }
