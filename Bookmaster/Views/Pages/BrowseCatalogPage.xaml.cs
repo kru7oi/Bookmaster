e2bc679ba54cbdb49a42325a4ec6d5011b01023e
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-17 18:34:03 +0300

    Revert "Добавлены имена, свойства, привязки и события"
    This reverts commit 83641b6ad3cce919b8847378d0cd54c38cc82b92.

--- Bookmaster/Views/Pages/BrowseCatalogPage.xaml.cs
+++ Bookmaster/Views/Pages/BrowseCatalogPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Bookmaster.AppData;
-using Bookmaster.Model;
+﻿using Bookmaster.Model;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -11,10 +10,13 @@ namespace Bookmaster.View.Pages
     public partial class BrowseCatalogPage : Page
     {
         private List<Book> _books = App.context.Books.ToList();
-        private PaginationService<Book>? _booksPagination;
         public BrowseCatalogPage()
         {
             InitializeComponent();
+
+            BookAuthorsLv.ItemsSource = _books;
+
+            BookDetailsGrid.Visibility = Visibility.Hidden;
         }
 
         private void BookAuthorsLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -24,8 +26,6 @@ namespace Bookmaster.View.Pages
 
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
         {
-            SearchResultsGrid.Visibility = Visibility.Visible;
-
             string bookTitle = SearchByBookTitleTb.Text;
             string authorName = SearchByAuthorNameTb.Text;
             string bookSubjects = SearchByBookSubjectsTb.Text;
@@ -34,42 +34,14 @@ namespace Bookmaster.View.Pages
                 string.IsNullOrEmpty(authorName) &&
                 string.IsNullOrEmpty(bookSubjects))
             {
-                _booksPagination = new PaginationService<Book>(_books, 50);
+                BookAuthorsLv.ItemsSource = _books;
             }
             else
             {
-                IEnumerable<Book> searchResult = _books.Where(book =>
+                BookAuthorsLv.ItemsSource = _books.Where(book =>
                 book.Title.Contains(bookTitle, StringComparison.OrdinalIgnoreCase) &&
                 book.Authors.Contains(authorName, StringComparison.OrdinalIgnoreCase));
-
-                _booksPagination = new PaginationService<Book>(searchResult.ToList(), 50);
             }
-
-            BookAuthorsLv.ItemsSource = _booksPagination.CurrentPageOfItems;
-            CurrentPageTb.Text = _booksPagination.CurrentPageNumber.ToString();
-            TotalPagesTbl.DataContext = TotalBooksTbl.DataContext = _booksPagination;
-            _booksPagination.UpdateNavigationButtons(NextBookBtn, PreviousBookBtn);
-        }
-        private void PreviousBookBtn_Click(object sender, RoutedEventArgs e)
-        {
-            BookAuthorsLv.ItemsSource = _booksPagination.PreviousPage();
-            CurrentPageTb.Text = _booksPagination.CurrentPageNumber.ToString();
-            _booksPagination.UpdateNavigationButtons(NextBookBtn, PreviousBookBtn);
-        }
-        private void NextBookBtn_Click(object sender, RoutedEventArgs e)
-        {
-            BookAuthorsLv.ItemsSource = _booksPagination.NextPage();
-            CurrentPageTb.Text = _booksPagination.CurrentPageNumber.ToString();
-            _booksPagination.UpdateNavigationButtons(NextBookBtn, PreviousBookBtn);
-        }
-        private void CurrentPageTb_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (int.TryParse(CurrentPageTb.Text, out int pageNumber) && pageNumber >= 1 && pageNumber <= _booksPagination.TotalPages)
-            {
-                BookAuthorsLv.ItemsSource = _booksPagination.SetCurrentPage(pageNumber);
-            }
-
-            _booksPagination.UpdateNavigationButtons(NextBookBtn, PreviousBookBtn);
         }
     }
 }
