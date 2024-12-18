a82cdeeb4a0f8bac52ebb18f297b09e872dbf5a3
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-18 20:41:29 +0300

    Реализован алгоритм поиска читателя по идентификатору и имени

--- Bookmaster/Views/Pages/ManageCustomersPage.xaml.cs
+++ Bookmaster/Views/Pages/ManageCustomersPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using Bookmaster.Model;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Bookmaster.View.Pages
 {
@@ -7,9 +9,40 @@ namespace Bookmaster.View.Pages
     /// </summary>
     public partial class ManageCustomersPage : Page
     {
+        private List<Customer> _customers = App.context.Customers.ToList();
         public ManageCustomersPage()
         {
             InitializeComponent();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CustomersLv.Visibility = Visibility.Visible;
+
+            string customerId = SearchCustomerByIdTb.Text;
+            string customerName = SearchCustomerByNameTb.Text;
+
+            if (string.IsNullOrEmpty(customerId) &&
+                string.IsNullOrEmpty(customerName))
+            {
+                CustomersLv.ItemsSource = _customers;
+            }
+            else
+            {
+                CustomersLv.ItemsSource = _customers.Where(customer =>
+                customer.Id.Contains(customerId, StringComparison.OrdinalIgnoreCase) &&
+                customer.Name.Contains(customerName, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+
+        private void EditCustomerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddCustomerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
