18f4d658781f3b499fdb863cba378f5db82dd62c
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-18 20:25:26 +0300

    Реализована функциональность для окна "Подробная информация об авторах".

--- Bookmaster/Views/Windows/BookAuthorsDetailsWindow.xaml.cs
+++ Bookmaster/Views/Windows/BookAuthorsDetailsWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using Bookmaster.AppData;
+using Bookmaster.Model;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace Bookmaster.View.Windows
 {
@@ -7,9 +12,38 @@ namespace Bookmaster.View.Windows
     /// </summary>
     public partial class BookAuthorsDetailsWindow : Window
     {
-        public BookAuthorsDetailsWindow()
+        public BookAuthorsDetailsWindow(ICollection<BookAuthor> bookAuthors)
         {
             InitializeComponent();
+
+            AuthorsCmb.ItemsSource = bookAuthors;
+            AuthorsCmb.SelectedIndex = 0;
+            AuthorsCmb.DisplayMemberPath = "Author.Name";
+        }
+
+        private void AuthorsCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataContext = AuthorsCmb.SelectedItem as BookAuthor;
+        }
+
+        private void WikipediaHl_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            string browserPath = @"C:\Users\Владислав\AppData\Local\Yandex\YandexBrowser\Application\browser.exe";
+
+            try
+            {
+                Process.Start(browserPath, e.Uri.AbsoluteUri);
+                e.Handled = true;
+            }
+            catch (Exception exception)
+            {
+                Feedback.Error(exception.Message);
+            }
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
