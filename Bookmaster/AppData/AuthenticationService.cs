5f7fa3fe2991d4e51b42ccc5d6c29b1754198b1a
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-18 20:18:47 +0300

    Реализован класс для аутентификации администраторов.

---
+++ Bookmaster/AppData/AuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using Bookmaster.Model;
+
+namespace Bookmaster.AppData
+{
+    static class AuthenticationService
+    {
+        public static Administrator? CurrentAdministrator { get; private set; }
+
+        public static bool Authenticate(string username, string password)
+        {
+            CurrentAdministrator = App.context.Administrators.FirstOrDefault(administrator => administrator.Username == username && administrator.Password == password);
+
+            if (CurrentAdministrator != null)
+            {
+                Feedback.Information("Администратор успешно авторизовался.");
+
+                return true;
+            }
+
+            Feedback.Error("Пользователь не найден! Возможно вы ввели неправильно имя пользователя или пароль. Попробуйте снова.");
+
+            return false;
+        }
+
+        public static void Logout()
+        {
+            CurrentAdministrator = null;
+            Feedback.Information("Вы вышли из системы.");
+        }
+    }
+}
