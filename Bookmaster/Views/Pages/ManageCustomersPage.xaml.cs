84cd5852cfabae1abb3e002d10dc86f104530357
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-12-18 20:48:08 +0300

    Добавлена функциональность для кнопок "Добавить" и "Редактировать".

--- Bookmaster/Views/Pages/ManageCustomersPage.xaml.cs
+++ Bookmaster/Views/Pages/ManageCustomersPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Bookmaster.Model;
+﻿using Bookmaster.AppData;
+using Bookmaster.Model;
+using Bookmaster.View.Windows;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -37,12 +39,26 @@ namespace Bookmaster.View.Pages
 
         private void EditCustomerBtn_Click(object sender, RoutedEventArgs e)
         {
+            Customer? selectedCustomer = CustomersLv.SelectedItem as Customer;
 
+            if (selectedCustomer != null)
+            {
+                AddEditCustomerWindow addEditCustomerWindow = new AddEditCustomerWindow(selectedCustomer);
+                addEditCustomerWindow.ShowDialog();
+            }
+            else
+            {
+                Feedback.Error("Невозможно открыть окно для редактирования читателя. Сначала выберите его из списка.");
+            }
         }
 
         private void AddCustomerBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            AddEditCustomerWindow addEditCustomerWindow = new AddEditCustomerWindow();
+            if (addEditCustomerWindow.ShowDialog() == true)
+            {
+                CustomersLv.ItemsSource = _customers;
+            }
         }
     }
 }
